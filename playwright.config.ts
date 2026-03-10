55d675d5f4d6dd8f5d2db7e352a30ea849077db5
Carlos Ramirez <>
2026-03-09 22:36:50 -0500

    test(taller7): Taller paralelismo, reportes y evidencias

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
+import { DateFormatter } from "./helpers/utils/time.helper";
+import { CustomDevice, CustomDeviceName } from "./data/CustomDevices";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
@@ -20,60 +11,48 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["list", { printSteps: true }]], //'html',
+  workers: process.env.CI ? 1 : 4,
+  /* Reporter to use. See https://playwright.dev/docs/test reporters */
+  reporter: [
+    ["list", { printSteps: true }],
+    [
+      "html",
+      {
+        open: "never",
+        outputFolder: `artifacts/playwright-report/result-${DateFormatter(new Date())}`,
+      },
+    ],
+    ["json", { outputFile: `artifacts/test-results/result-${DateFormatter(new Date())}.json` }],
+  ],
+
+  // Ruta donde se guardará evidencias de cada ejecución (videos, traces, screenshots)
+  outputDir: "artifacts/run",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    launchOptions: {
+      slowMo: 500,
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    /*
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
+
+    // === Dispositivos móviles (emulación) ===
+    { name: "Mobile Chrome", use: { ...devices["Pixel 5"] } },
+    { name: "Mobile Safari", use: { ...devices["iPhone 15 Pro"] } },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: CustomDeviceName.CustomIphone,
+      use: { ...CustomDevice[CustomDeviceName.CustomIphone] },
     },
-*/
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
---
+++ tests/taller7/suiteOnlyDefinedDevice.spec.ts
@@ -0,0 +1,64 @@
+import { test, expect } from "@playwright/test";
+import { CustomDevice, CustomDeviceName } from "../../data/CustomDevices";
+
+test.describe("T7 · Emulación solo custom device y APIRequestContex", () => {
+  test("Test — solo disponible para custom device", async ({ page, isMobile, request }) => {
+    const projectName = test.info().project.name;
+
+    // Salta inmediatamente si NO es el proyecto requerido
+    test.skip(
+      projectName !== CustomDeviceName.CustomIphone,
+      `Este test sólo corre en "${CustomDeviceName.CustomIphone}". Proyecto actual: "${projectName}".`,
+    );
+
+    // Asserts para validar navegador
+    const vp = page.viewportSize();
+    expect(vp).toBeTruthy();
+    expect(vp?.width).toBe(CustomDevice[CustomDeviceName.CustomIphone].viewport.width);
+    expect(vp?.height).toBe(CustomDevice[CustomDeviceName.CustomIphone].viewport.height);
+    expect(isMobile).toBe(true);
+
+    // APIRequestContext
+
+    await test.step("API · GET", async () => {
+      const resp = await request.get("https://playground.mockoon.com/companies");
+      expect(resp.status()).toBe(200);
+
+      const body = await resp.json();
+      expect(Array.isArray(body)).toBeTruthy();
+      expect(body.length).toBeGreaterThan(0);
+
+      // Validación de Campos críticos
+      expect(body[0]).toHaveProperty("id");
+      expect(body[0]).toHaveProperty("name");
+    });
+
+    await test.step("API · POST", async () => {
+      const objectToInsert = {
+        name: "Test e2e",
+        industry: "example",
+        location: {
+          city: "South Valley",
+          country: "American Samoa",
+        },
+        employees: 6,
+        is_public: false,
+      };
+      const resp = await request.post("https://playground.mockoon.com/companies", {
+        data: objectToInsert,
+      });
+
+      expect(resp.status()).toBe(201);
+
+      const body = await resp.json();
+      expect(body).toMatchObject(objectToInsert);
+      expect(body).toHaveProperty("id");
+    });
+
+    // Acciones UI
+    await test.step("UI · Navegar y validar título", async () => {
+      await page.goto("https://playwright.dev/");
+      await expect(page).toHaveTitle(/Playwright/);
+    });
+  });
+});
