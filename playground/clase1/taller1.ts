4c88d78c0b67121dafa5e6be71087082358fbd95
malinda <malindaperez@hotmail.com>
2026-08-16 00:42:33 -0500

    test(config): clase 1 : estructura base y configuración inicial

---
+++ playground/clase1/taller1.ts
@@ -0,0 +1,22 @@
+const curso = "Playwright";
+let alumno = "Malinda";
+const notas = [15, 18, 14, 20];
+
+function mostrarAlumno(nombre : string) {
+console.log("Alumno:", nombre);
+}
+
+mostrarAlumno(alumno);
+if (notas[0] >= 11) {
+console.log("Aprobado");
+} else {
+console.log("Desaprobado");
+}
+
+for (const i in notas) {
+console.log("Nota", i, ":", notas[i]);
+}
+
+for (const nota of notas) {
+console.log(nota);
+}
