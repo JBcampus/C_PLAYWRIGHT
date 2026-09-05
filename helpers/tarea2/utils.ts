5f19f0c123b2b0e855c69847083d5de74f73b0f6
KARLA T <wkarla.navarro@yahoo.com>
2026-09-05 13:14:02 -0500

    taller6

---
+++ helpers/tarea2/utils.ts
@@ -0,0 +1,14 @@
+ 
+export function getValidCredentials() {
+  return {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  };
+}
+
+export function getLockedOutCredentials() {
+  return {
+    username: 'locked_out_user',
+    password: 'secret_sauce',
+  };
+}
